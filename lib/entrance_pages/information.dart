eeb5fca901f49815a3caf66aa124582a46180340
yalpay <yalpay38@gmail.com>
2024-03-30 16:58:26 +0300

    info update

--- lib/entrance_pages/information.dart
+++ lib/entrance_pages/information.dart
@@ -32,18 +32,18 @@ class Info extends State<InformationScreen> {
                   padding: EdgeInsets.all(10),
                   child: Text(
                       style: TextStyle(fontSize: 15),
-                      "Bu uygulama ile Kur'anı Kerim den ezberlerinizi test edebilirsiniz. Hafız olanlar mevcut ezberlerini gözden geçirebilir, "
-                      "hafızlık sınavlarına hazırlanabilirler.\n\nUygulama şu şekilde çalışır: İstenen cüzden, istenen sureden veya tüm Kur'an dan "
-                      "karşınıza rasgele bir ayet çıkar ve sizden bir sonraki ayeti tamamlamanız beklenir. Otomatik olarak sayfa başından bir ayet "
-                      "çıkar ve karşınıza çıkan ayeti önce Kabe imamından dinlersiniz. Ardından dilerseniz ses kaydı başlatırsınız ve sonraki ayeti "
-                      "okumaya başlarsınız. Ses kaydı başlatmanız durumunda uygulama sesinizi kaydeder ve sonraki ayet ile karşılaştırır. Yanlış "
-                      "okuma durumunda yanlış okuduğunuz kısım gösterilir. Bu bölümde mahreçlere dikkat ederek, tecvid kuralları ile okumak önemlidir. "
-                      "Ayarlar kısmında ayeti Kabe imamından dinleme özelliğini kaldırabilir, veya okuyuş hızını ayarlayabilirsiniz. Rasgele çıkan "
-                      "ayetin sayfa başından olup olmamasını ayarlayabilirsiniz. Test ekranında ayetin bulunduğu sayfanın tamamını görüntüleyebilir, "
-                      "bir sonraki veya bir önceki ayete hızlı geçiş yapabilirsiniz. Ezberinizin kuvvetli olduğu bölümleri favorilere alabilir, test "
-                      "bölümünde bu kısımlardan sorulmamasını sağlayabilirsiniz. Örneğin 15. cüzünüz sağlam ise bu cüzü favoriledikten sonra 'Bütün "
-                      "Kur'an dan Sor' seçeneğini tıklarsanız bu cüzden soru gelmeyecektir. Benzer şekilde 1.cüzün 1.dönüşü, yani 20. sayfanız sağlam "
-                      "ise bu sayfayı favorileyerek bu sayfadan soru gelmemesini sağlayabilirsiniz.\nAllah okuduklarımızla amel edebilmeyi nasip eylesin"),
+                      "Bu uygulama ile Kur'anı Kerim den ezberlerinizi test edebilirsiniz. Hafız olanlar mevcut ezberlerini gözden geçirebilir, hafızlık sınavlarına "
+                      "hazırlanabilirler.\n\nUygulama şu şekilde çalışır: İstenen cüzden, istenen sureden veya tüm Kur'an dan karşınıza rasgele bir ayet çıkar ve "
+                      "sizden bir sonraki ayeti tamamlamanız beklenir. Otomatik olarak sayfa başından bir ayet çıkar ve karşınıza çıkan ayeti önce Kabe imamından "
+                      "dinlersiniz. Ardından dilerseniz ses kaydı başlatırsınız ve sonraki ayeti okumaya başlarsınız. Ses kaydı başlatmanız durumunda uygulama "
+                      "sesinizi kaydeder ve sonraki ayet ile karşılaştırır. Yanlış okuma durumunda yanlış okuduğunuz kısım gösterilir. Bu bölümde mahreçlere "
+                      "dikkat ederek, tecvid kuralları ile okumak önemlidir. (İnternetiniz kapalı iken uygulamanın dinleme ve kayıt özellikleri çalışmaz) Ayarlar "
+                      "kısmında ayeti Kabe imamından dinleme özelliğini kaldırabilir, veya okuyuş hızını ayarlayabilirsiniz. Rasgele çıkan ayetin sayfa başından "
+                      "olup olmamasını ayarlayabilirsiniz. Test ekranında ayetin bulunduğu sayfanın tamamını görüntüleyebilir, bir sonraki veya bir önceki ayete "
+                      "hızlı geçiş yapabilirsiniz. Ezberinizin kuvvetli olduğu bölümleri favorilere alabilir, test bölümünde bu kısımlardan sorulmamasını "
+                      "sağlayabilirsiniz. Örneğin 15. cüzünüz sağlam ise bu cüzü favoriledikten sonra 'Bütün Kur'an dan Sor' seçeneğini tıklarsanız bu cüzden "
+                      "soru gelmeyecektir. Benzer şekilde örneğin 1.cüzün 1.dönüşü, yani 20. sayfanız sağlam ise bu sayfayı favorileyerek bu sayfadan soru "
+                      "gelmemesini sağlayabilirsiniz.\nAllah okuduklarımızla amel edebilmeyi nasip eylesin"),
                 ),
               ],
             ),
